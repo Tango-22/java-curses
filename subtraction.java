cfe9b16019c0eeb016339456d601df6e57bc7d58
Tango-22 <Gall2019@yandex.ru>
2017-07-08 15:18:30 +0500

    create subtraction

---
+++ subtraction.java
@@ -0,0 +1,11 @@
+public class subtraction {
+
+       public static void main(String[] arg) {
+              System.out.println("Calculate...");
+              double first = Double.valueOf(arg[0]);
+              double second = Double.valueOf(arg[1]);
+              double sub = first - second;
+              System.out.println("Sub = " + sub);      
+       }     
+
+}
